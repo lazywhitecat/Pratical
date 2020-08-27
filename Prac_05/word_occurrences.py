e1537be1543a9410acbac8a35ebc53003477a4a7
lazywhitecat <junjiequ@my.jcu.edu.au>
2020-08-27 15:34:11 +0800

    update changes

--- Prac_05/word_occurrences.py
+++ Prac_05/word_occurrences.py
@@ -1,7 +1,7 @@
 word_list = input('Enter your string:')
 data = word_list.split(' ')
 word_dict = {word:0 for word in data}
-for keys in word_dict:
+for keys in data:
     word_dict[keys]+=1
 for list1,list2 in word_dict.items():
     print('{} - {}'.format(list1,list2))
