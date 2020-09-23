8afcace5a9942d86b218b046c422e2d1f603bc9e
lazywhitecat <junjiequ@my.jcu.edu.au>
2020-09-23 22:49:46 +0800

    initial commit

---
+++ Prac_09/sort_files_1.py
@@ -0,0 +1,17 @@
+import os
+
+def main():
+    os.chdir("sort_file")
+    for file_name in os.listdir('.'):
+        if os.path.isdir(file_name):
+            continue
+        extension = file_name.split('.')[-1]
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            pass
+        print("{}/{}".format(extension, file_name))
+        os.rename(file_name, "{}/{}".format(extension, file_name))
+
+
+main()
